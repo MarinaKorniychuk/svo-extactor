bac34d0ac07cea8a7c4c563c3a080d729a27daf0
Marina Korniychuk <kornij4uk@gmail.com>
2019-08-19 17:20:42 +0300

    refactoring and small updates to docstrings

--- facts/extractors/components.py
+++ facts/extractors/components.py
@@ -6,7 +6,9 @@ from facts.settings import TOKENS_TO_FILTER
 
 
 def remove_tokens_on_match(doc):
-    """Filter out stop word tokens, while maintaining sentences boundaries."""
+    """Filter out stop word tokens, while maintaining sentences boundaries.
+
+    Note: should be added to pipeline after tagger, but before parser component."""
     indices = []
     for index, token in enumerate(doc):
         if token.pos_ in TOKENS_TO_FILTER:
@@ -23,15 +25,18 @@ def remove_tokens_on_match(doc):
 
 
 def crop_to_two_sentences(doc):
-    """Crop the text to two or fewer sentences."""
+    """Crop the text to two sentences.
+    Do not affect doc if there are only two or fewer sentences.
+
+    Note: should be added to pipeline after sentencizer,
+    so that sentence boundaries are already set, but before parser component."""
     np_array = doc.to_array(SENT_START)
     sent_start_indices = np.where(np_array == 1)[0]
 
-    if len(sent_start_indices) > 2:
-        fragment_len = sent_start_indices[2]
-    else:
-        fragment_len = len(np_array)
+    if len(sent_start_indices) <= 2:
+        return doc
 
+    fragment_len = sent_start_indices[2]
     words = [t.text for i, t in enumerate(doc[:fragment_len])]
     doc2 = Doc(doc.vocab, words=words)
     doc2.from_array(SENT_START, np_array[:fragment_len])
