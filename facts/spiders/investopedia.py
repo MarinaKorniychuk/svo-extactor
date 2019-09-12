aad4348470ed0284cff6ed4346483b152064b9c5
Marina Korniychuk <kornij4uk@gmail.com>
2019-09-12 16:53:39 +0300

    parse updated investopedia content
    replace index selectors with classes
    because index includes version and can be changed by updates

--- facts/spiders/investopedia.py
+++ facts/spiders/investopedia.py
@@ -17,32 +17,19 @@ class InvestopediaSpider(scrapy.Spider):
 
     def parse(self, response):
         """ Parse a response from Home page."""
-        for link in response.css("#terms-bar__list_1-0 a"):
+        for link in response.css("li.terms-bar__item a"):
             yield response.follow(url=link, callback=self.parse_list)
 
     def parse_list(self, response):
         """Parse a response from Terms Beginning With "[symbol]" page."""
-        for detail_link in response.css("li.item a"):
+        for detail_link in response.css(".dictionary-listing a"):
             yield response.follow(url=detail_link, callback=self.parse_detail)
-
-        next_page_url = response.css("li.next a::attr(href)").get()
-
-        if next_page_url:
-            yield response.follow(url=next_page_url, callback=self.parse_list)
 
     def parse_detail(self, response):
         """Parse a response from term Detail page."""
-        # There are some terms in the dictionary for which Detail page does not exist,
-        # Request like this are redirected to other pages (e.g Investing Essentials)
-        if response.meta.get("redirect_urls"):
-            self.logger.warning(
-                f'Redirected (301) from <GET {response.meta["redirect_urls"][0]}> to <GET {response.url}>: SKIPPING'
-            )
-            return
-
-        text = "".join(response.css("#article-body_1-0 p")[0].css("p *::text").getall())
+        text = "".join(response.css(".article-body p")[0].css("p *::text").getall())
         yield {
             "url": response.url,
-            "title": response.css("h1#article-heading_2-0::text").get().strip(),
+            "title": response.css("h1.article-heading::text").get().strip(),
             "text": text.replace("\xa0", " ").strip(),
         }
